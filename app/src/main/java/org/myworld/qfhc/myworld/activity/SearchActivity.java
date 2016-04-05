98ef40b10a19a83ffeb2b698be50af513aba1cfc
NO1f <hc1251249582@sina.com>
2016-04-05 08:56:52 +0800

    third

---
+++ app/src/main/java/org/myworld/qfhc/myworld/activity/SearchActivity.java
@@ -0,0 +1,16 @@
+package org.myworld.qfhc.myworld.activity;
+
+import org.myworld.qfhc.myworld.R;
+import org.myworld.qfhc.myworld.base.BaseActivity;
+
+/**
+ * @类描述: ${TODO}
+ * @创建时间：2016/4/5 01:26
+ * @备注：
+ */
+public class SearchActivity extends BaseActivity {
+    @Override
+    protected int getContentResid() {
+        return R.layout.search_layout;
+    }
+}
